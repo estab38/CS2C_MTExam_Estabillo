4b23adb1dac64664886e9c1ae8f32b53b84c4f4a
Estabillo Ermalyn <ermalyncalindas19@gmail.com>
2024-10-31 10:31:13 +0800

    Create userProfile.js

---
+++ userProfile.js
@@ -0,0 +1,9 @@
+let fullName = ("Ermalyn C. Eatabillo:");
+let age = ("20:");
+let favoriteNumber = ("13:");
+let favoriteColor = ("Black:");
+
+console.log("Full Name:", fullName);
+console.log("Age:", age);
+console.log("Favorite Number:", favoriteNumber);
+console.log("Favorite Color:", favoriteColor);
