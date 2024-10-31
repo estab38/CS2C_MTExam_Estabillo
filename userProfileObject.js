e8c1b4b265f0456c3b91068f2636c94a365d7a22
Estabillo Ermalyn <ermalyncalindas19@gmail.com>
2024-10-31 11:13:01 +0800

    Create userProfileObject.js

---
+++ userProfileObject.js
@@ -0,0 +1,12 @@
+let userProfile = {
+  fullName: ("Ermalyn C. Estabillo:").trim(),
+  age: parseInt(("20:").trim()),
+  favoriteNumber: parseInt(("19:").trim()),
+  favoriteColors: [
+    ("Black:").trim(),
+    ("White:").trim(),
+    ("Red:").trim(),
+  ],
+};
+
+console.log(userProfile);
