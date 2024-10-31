eb62d990f325a602c5f24b46c4ae8906ec55b88d
Estabillo Ermalyn <ermalyncalindas19@gmail.com>
2024-10-31 11:06:15 +0800

    Create userSubjects.js

---
+++ userSubjects.js
@@ -0,0 +1,7 @@
+let subjectTitle = ("Data Structures and Algorithms:");
+let classSchedule = ("10:00-11:00, Thursday:");
+let classroom = ("CLB:");
+let classInstructor = ("Cliff Owen Pascua:");
+let studentName = ("Ermalyn C. Estabillo:");
+
+console.log(`${studentName} is currently enrolled in ${subjectTitle} with a class schedule of ${classSchedule} at room ${classroom}. The instructor for the subject is ${classInstructor}`);
