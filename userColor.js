c9de99b1b782fddda708fe8f641ee949da9abcb4
Estabillo Ermalyn <ermalyncalindas19@gmail.com>
2024-10-31 11:11:09 +0800

    Create userColor.js

---
+++ userColor.js
@@ -0,0 +1,11 @@
+let colors = [];
+
+colors.push(("Black:"));
+colors.push(("White:"));
+colors.push(("Red:"));
+
+console.log("Your favorite colors are:", colors);
+
+colors.push(("Brown:"));
+
+console.log("Your updated favorite colors are:", colors);
